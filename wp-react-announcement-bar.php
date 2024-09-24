3ea2211f56cdc91f448fca6b56ea20dbf69989d9
Shamim Mia <shamimshuhagh@gmail.com>
2024-09-24 11:07:19 +0600

    Add the Plugin main files and src skeleton before installing wp-scripts

---
+++ wp-react-announcement-bar.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * Plugin Name:       WP React Announcement Bar
+ * Description:       A Testing Plugin to practice React with WordPress Plugin Page. This plugin will add an announcement bar to website if we enable the option form setting page.
+ * Requires at least: 6.6
+ * Requires PHP:      7.0
+ * Version:           0.1.0
+ * Author:            Shamim Mia
+ * Plugin URI:       https://shamimmia.com
+ * Author URI:       https://shamimmia.com
+ * License:           GPL-2.0-or-later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain:       wp-react-announcement-bar
+ *
+ * @package CreateBlock
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
+
+/**
+ * Setting Page.
+ *
+ * @return void
+ */
+function shamim_wp_react_announcement_bar() {
+	add_options_page( __( 'React Announcement Bar', 'wp-react-announcement-bar' ), __( 'React Announcement Bar', 'wp-react-announcement-bar' ), 'manage_options', 'wp-react-announcement-bar', 'wp_react_announcement_bar_setting_page_html' );
+}
+
+add_action( 'admin_menu', 'shamim_wp_react_announcement_bar' );
+
+/**
+ * Settings Page HTML
+ *
+ * @return void
+ */
+function wp_react_announcement_bar_setting_page_html() {
+	printf( '<div class="wrap" id="shamim-announcement-bar-settings">%s</div>', esc_html__( ' Loading...', 'wp-react-announcement-bar' ) );
+}
+
+
+function wp_react_announcement_bar_setting_page_enqueue_style_script( $admin_page ) {
+	if ( 'wp-react-announcement-bar' !== $admin_page ) {
+		return;
+	}
+
+	$asset_file = plugin_dir_path( __FILE__ ) . 'build/index.asset.php'
+}
